c992af9621854ae0b6edf5744c1b494f2d31490a
Md. Ashikur Rahman <ashikur.rahman@konasl.com>
2025-12-23 14:04:29 +0600

    Updated the pause statement

--- tests/pages-card-application.spec.js
+++ tests/pages-card-application.spec.js
@@ -9,13 +9,13 @@ import { submitApplication } from '../pages/submit.page';
 
 
 test('Card Application End-to-End', async ({ page }) => {
-  //Run steps from here, use 'pauseTheTest;' to pause the test at any point
-  //const pauseTheTest = await page.pause(); // Uncomment this line to pause the test for debugging
 
   await login(page); //Login to the CMS Portal
   await fillCardAppPage1(page); // Fill Applicant Details of Card Application
   await fillCardAppPage2(page); // Fill Identification & Address of Card Application
-  await page.pause();
+
+  //await page.pause(); // >>>>>>>>>>>>>>>>>>To Pause the test for debugging, place it where needed.
+  
   await fillCardAppPage3(page); // Fill Product & Bank Information of Card Application
   await fillCardAppPage4(page); // Fill Nominee Information of Card Application
   // Submit isolated
