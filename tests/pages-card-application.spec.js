72c8ae5763b7b7f19beaab1b83b293b0f8ef965e
Md. Ashikur Rahman <ashikur.rahman@konasl.com>
2025-12-23 12:53:36 +0600

    Adding necessary comment in the page 3

--- tests/pages-card-application.spec.js
+++ tests/pages-card-application.spec.js
@@ -8,13 +8,13 @@ import { fillCardAppPage4 } from '../pages/cardApp.page4';
 import { submitApplication } from '../pages/submit.page';
 
 test('Card Application End-to-End', async ({ page }) => {
-  await login(page);
 
-  await fillCardAppPage1(page);
-  await fillCardAppPage2(page);
-  await fillCardAppPage3(page);
-  await fillCardAppPage4(page);
-
+  
+  await login(page); //Login to the CMS Portal
+  await fillCardAppPage1(page); // Fill Applicant Details of Card Application
+  await fillCardAppPage2(page); // Fill Identification & Address of Card Application
+  await fillCardAppPage3(page); // Fill Product & Bank Information of Card Application
+  await fillCardAppPage4(page); // Fill Nominee Information of Card Application
   // Submit isolated
-  await submitApplication(page);
+  await submitApplication(page); //Preview and Submit the Card Application
 });
