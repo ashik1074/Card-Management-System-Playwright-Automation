e350528a0811d1e7066046bd859d7673cbed08a6
Md. Ashikur Rahman <ashikur.rahman@konasl.com>
2025-12-24 09:31:36 +0600

    Moving the pause command into pauseHere function

--- pages/submit.page.js
+++ pages/submit.page.js
@@ -1,4 +1,4 @@
 export async function submitApplication(page) {
-  await page.getByRole('button', { name: 'Submit Application' }).click();
+  //await page.getByRole('button', { name: 'Submit Application' }).click();
   await page.pause();
 }
--- tests/pages-card-application.spec.js
+++ tests/pages-card-application.spec.js
@@ -10,15 +10,18 @@ import { submitApplication } from '../pages/submit.page';
 
 test('Card Application End-to-End', async ({ page }) => {
 
+  const pauseHere = async () => {
+    await page.pause();
+  };
+
+  //To Pause the test for debugging, place it where needed.  
+  //await pauseHere();  
+
   await login(page); //Login to the CMS Portal
   await fillCardAppPage1(page); // Fill Applicant Details of Card Application
   await fillCardAppPage2(page); // Fill Identification & Address of Card Application
-
-  //await page.pause(); // >>>>>>>>>>>>>>>>>>To Pause the test for debugging, place it where needed.
-  
   await fillCardAppPage3(page); // Fill Product & Bank Information of Card Application
   await fillCardAppPage4(page); // Fill Nominee Information of Card Application
-  // Submit isolated
   await submitApplication(page); //Preview and Submit the Card Application
 
 });
