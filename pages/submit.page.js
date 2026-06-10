41efec9315f8e330abe85ecff2207abf5738ccc4
Md. Ashikur Rahman <ashikurrahman1052@gmail.com>
2026-06-10 09:32:55 +0600

    Removing commented line

--- pages/submit.page.js
+++ pages/submit.page.js
@@ -2,6 +2,5 @@ import { fullName } from '../pages/cardApp.page1';
 
 export async function submitApplication(page) {
   //await page.getByRole('button', { name: 'Submit Application' }).click();
-  //console.log('Submitting application for:', fullName); // Log the name being submitted
   await page.pause();
 }
